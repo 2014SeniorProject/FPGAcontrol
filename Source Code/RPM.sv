b5ad345feaf5a3f74fe9e8b99793dde2eca579cd
Benjamin Smith <Benjamin.smith.cs@gmail.com>
2013-11-11 15:19:26 -0800

    Commenting and indentation fix

--- Source Code/RPM.sv
+++ Source Code/RPM.sv
@@ -3,7 +3,7 @@
 //|     Authors: Michael Frith
 //|
 //|     This module calculates the RPM of the electric motor.
-//|    
+//|
 
 //| Uncomment the `include "debug.sv" to enter debug mode on this module.
 //| Uncomment the `include "timescale.sv" to run a simulation.
@@ -24,37 +24,35 @@ module RPM(
 	reg [31:0] 	clkstore=0;          //register to store the clock counter while maths happen
 	reg [32:0] 	numerator=0;             //numerator of the dimensional analysis equation
 	reg [31:0]	denominator=0;           //denominator of the dimensional analysis equation
-	reg					firstPass = 0;				
-	
+	reg					firstPass = 0;
+
 
 	//	clock counter, always counts at posedge of the clock,
 	//	gets reset on posedge of blips
-
 	always @(posedge clk50M)
 		begin
 			clkcount = clkcount +1;
-	
 
-	//	stores and resets count when motor controller sends in blips,
-	//	a square wave with period equal to pole changes/seconds,
-	//	does math on stored value, spits out RPM value
+			//	stores and resets count when motor controller sends in blips,
+			//	a square wave with period equal to pole changes/seconds,
+			//	does math on stored value, spits out RPM value
 			if(blips && !firstPass)
 				begin
-				
 					clkstore = clkcount;
 					clkcount = 32'd0;
 					denominator = clkstore*motor_pole_count*internal_gear_reduction_ratio;
 					numerator = clkspeed*60;
 					rpm = 0;
-			// dimensional analysis equation
-			//  1 blip  |     1 clock      | 60 seconds |  1 pole | 1 internal rotation | 1 external revolution
-			// _________________________________________________________________________________________________________  = RPM
-			//  x clocks| clock speed^-1(s)| 1 minute   | 1 blip  | number of poles     | internal gear  reduction ratio
+
+					// dimensional analysis equation
+					//  1 blip  |     1 clock      | 60 seconds |  1 pole | 1 internal rotation | 1 external revolution
+					// _________________________________________________________________________________________________________  = RPM
+					//  x clocks| clock speed^-1(s)| 1 minute   | 1 blip  | number of poles     | internal gear  reduction ratio
 					rpm = numerator/denominator;
 					firstPass = 1;
-				end  
-				
+				end
+
 				if (!blips) firstPass = 0;
 		end
-		
+
 endmodule
