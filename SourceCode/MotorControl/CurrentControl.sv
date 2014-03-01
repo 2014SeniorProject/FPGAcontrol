cd1335017e30f5f8bcc472ebf52e2f75b6571dff
Benjamin Smith <Benjamin.smith.cs@gmail.com>
2014-02-28 20:56:15 -0800

    more comments about signed math craziness

--- SourceCode/MotorControl/CurrentControl.sv
+++ SourceCode/MotorControl/CurrentControl.sv
@@ -14,30 +14,36 @@ module CurrentControl(
 
 	logic	[14:0] 	clkCount;
 	logic	[2:0]	MotorState;
-	
+
+    //| discard the lowest four bits of the ADC reading(probably should do in ADC module to save routing resources)
     assign ADCAdjustedReading = PhaseWireVoltage>>4;
-    assign AssistanceAdjusted = (AssistanceRequirement>>4);
-    assign DeltaTorque = signed'(AssistanceAdjusted) - signed'(ADCAdjustedReading);
-	
-	
+    assign AssistanceAdjusted = AssistanceRequirement>>4;
+
+    assign DeltaTorque = signed'(AssistanceAdjusted) - signed'(ADCAdjustedReading); // calculate the required speed settings
+
+
     ADCReadback ADCReadback_inst0 (.probe (ADCAdjustedReading));
     ADCReadback ADCReadback_inst1 (.probe (AssistanceAdjusted));
 	ADCReadback ADCReadback_inst2 (.probe (DeltaTorque));
     ADCReadback ADCReadback_inst3 (.probe (MotorSignal));
 	ADCReadback ADCReadback_inst4 (.probe (MotorState));
 
-	always @(posedge c20k)
-    clkCount++;
-	
+    //| clock divider register for debugging
+	always @(posedge c20k) clkCount++;
+
+                                                    //| type cast for unsigned numbers,      |10 bit signed number used for comparison             |<bitwidth>'<signed><format><number>
+                                                    //| preceding zero is required for       |if 8 bit is used the comparison can                  | constants must be specified as signed
+                                                    //| proper sign extension                |overflow causing invalid operation                   | if all operands are not signed Verilog
+                                                    //|                                      |                                                     | will default to unsigned operations
 	assign MotorState = {AssistanceAdjusted > 8'd0, signed'({1'b0,MotorSignal})+DeltaTorque > 10'sd0255, signed'({1'b0,MotorSignal})+DeltaTorque < 10'sd00};
 
-	always @(posedge clkCount[7])
-		begin
-		   casex(MotorState)
-				3'b100 :MotorSignal <= signed'({1'b0,MotorSignal}) + (DeltaTorque/PorportionalityConstant);
-				3'b110 :MotorSignal <= 8'd255;
-				3'b101 :MotorSignal <= 8'd0;
-				default:MotorSignal <= 8'd0;
-			endcase
-		end
+    //| Calculates motor velocity setting
+	always_comb @(posedge clkCount[7]) begin
+	   casex(MotorState)
+			3'b100 :MotorSignal <= signed'({1'b0,MotorSignal}) + (DeltaTorque/PorportionalityConstant);
+			3'b110 :MotorSignal <= 8'd255;
+			3'b101 :MotorSignal <= 8'd0;
+			default:MotorSignal <= 8'd0;
+		endcase
+	end
 endmodule
